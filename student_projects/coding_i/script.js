a4080648ad4606e45d1ee9629323c695c1080438
Caleb Gilmore <116288621+TeachingSTEMisCool@users.noreply.github.com>
2022-11-15 01:45:46 +0000

    updated table

--- student_projects/coding_i/script.js
+++ student_projects/coding_i/script.js
@@ -17,41 +17,52 @@ const dbRef = ref(database);
 loadDropdown('years');
 
 function createTable(){
-  const parent = 'coding_i/' + document.getElementById('dropdown').value;
-  get(child(dbRef, parent)).then((snapshot) => {
-    if (snapshot.exists()) {
-      const data = snapshot.val();
-      const table = document.createElement("table");
-      const header_row = document.createElement("tr");
-      const name_header = document.createElement("th");
-      name_header.innerHTML = "Name";
-      header_row.appendChild(name_header);
-      const link_header = document.createElement("th");
-      link_header.innerHTML = "Project Link";
-      header_row.appendChild(link_header);
-      table.appendChild(header_row);
-      const keys = Object.keys(data);
-      let i = 0;
-      for (const item in data){
-        let table_row = document.createElement("tr");
-        let row_name = document.createElement("td");
-        row_name.innerHTML = keys[i];
-        table_row.appendChild(row_name);
-        let row_link = document.createElement("td");
-        row_link.innerHTML = data[item];
-        table_row.appendChild(row_link);
-        table.appendChild(table_row);
-        i++;
+  const selection = document.getElementById('dropdown').value;
+  if (selection){
+    const parent = 'coding_i/' + selection;
+    get(child(dbRef, parent)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        const table = document.createElement("table");
+        const header_row = document.createElement("tr");
+        const name_header = document.createElement("th");
+        name_header.innerHTML = "Name";
+        header_row.appendChild(name_header);
+        const type_header = document.createElement("th");
+        type_header.innerHTML = "Project Type";
+        header_row.appendChild(type_header);
+        const link_header = document.createElement("th");
+        link_header.innerHTML = "Project Link";
+        header_row.appendChild(link_header);
+        table.appendChild(header_row);
+        const keys = Object.keys(data);
+        let i = 0;
+        for (const item in data){
+          let table_row = document.createElement("tr");
+          let row_name = document.createElement("td");
+          row_name.innerHTML = keys[i];
+          row_name.width = "120px";
+          table_row.appendChild(row_name);
+          let row_type = document.createElement("td");
+          row_type.innerHTML = data[item].type;
+          row_type.width = "120px";
+          table_row.appendChild(row_type);
+          let row_link = document.createElement("td");
+          row_link.innerHTML = data[item].link;
+          table_row.appendChild(row_link);
+          table.appendChild(table_row);
+          i++;
+        }
+        const div = document.getElementById("tableDiv");
+        div.innerHTML = "";
+        div.appendChild(table);
+      } else {
+        console.log("No data available");
       }
-      const div = document.getElementById("tableDiv");
-      div.innerHTML = "";
-      div.appendChild(table);
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
 }
 
 function loadDropdown(course){
